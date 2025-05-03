2ec46e0f264ad9b81eb993df513fbe398953f146
Amrit Singh <88510705+Amrit1997@users.noreply.github.com>
2025-05-03 23:47:02 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -9,7 +9,7 @@ resource "azurerm_subnet" "subnet" {
   name                 = "${var.vm_name}-vnet"
   resource_group_name  = var.resource_group_name
   virtual_network_name = azurerm_virtual_network.vnet.name
-  address_prefixes     = "[10.0.0.0/24]"
+  address_prefixes     = ["10.0.0.0/24"]
 }
 
 resource "azurerm_public_ip" "puiblicip" {
